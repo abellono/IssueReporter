0e9160cf1676de51bd5203a9aa1b2eb67813332e
Hakon Hanesand <hakon@abello.no>
2016-10-18 12:29:54 -0700

    Fix incorrect merge

--- Example/Pods/Target Support Files/IssueReporter/IssueReporter-umbrella.h
+++ Example/Pods/Target Support Files/IssueReporter/IssueReporter-umbrella.h
@@ -1,22 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "ABEGithubAPIClient.h"
-#import "ABEImageCollectionViewCell.h"
-#import "ABEImageCollectionViewController.h"
-#import "ABEImgurAPIClient.h"
-#import "ABEIssue.h"
-#import "ABEIssueManager.h"
-#import "ABEReporter.h"
-#import "ABEReporterViewController.h"
-#import "NSBundle+ABEBundle.h"
-#import "NSFileManager+ABEFileManager.h"
-#import "NSThread+ABEMainThreadRunner.h"
-#import "NSURL+ABERandomImageURL.h"
-#import "UIAlertController+ABEErrorAlertController.h"
-#import "UIBarButtonItem+ABECustomBarButton.h"
-#import "UIColor+ABEColor.h"
-#import "UIImage+ABEAutoRotation.h"
-#import "UIWindow+ABEShakeNotifier.h"
 
 FOUNDATION_EXPORT double IssueReporterVersionNumber;
 FOUNDATION_EXPORT const unsigned char IssueReporterVersionString[];
